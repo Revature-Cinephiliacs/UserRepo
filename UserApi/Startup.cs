065b75d1cd54a4700078f7c8e1f77cacb940f312
T Matthew Grimsley <timothy.grimsley@revature.net>
2021-04-20 15:03:30 -0400

    Added repo class

--- UserApi/Startup.cs
+++ UserApi/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Repository.Models;
 
 namespace UserApi
 {
@@ -26,8 +28,24 @@ namespace UserApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                                builder =>
+                                {
+                                    builder.AllowAnyOrigin()
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                                });
+            });
 
             services.AddControllers();
+
+            var myConnectionString = Configuration.GetConnectionString("Cinephiliacs_User");
+            services.AddDbContext<Cinephiliacs_UserContext>(
+                options => options.UseSqlServer(myConnectionString)
+            );
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserApi", Version = "v1" });
@@ -48,6 +66,8 @@ namespace UserApi
 
             app.UseRouting();
 
+            // Enables the CORS policty for all controller endpoints. Must come between UseRouting() and UseEndpoints()
+            app.UseCors();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
