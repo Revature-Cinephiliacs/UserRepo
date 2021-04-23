08ea8df7ffaceacdddb30d87216b93927a347252
unknown <nicholasvvong@gmail.com>
2021-04-22 17:51:22 -0700

    Finished base user unit test

--- Testing/UserLogicTest.cs
+++ Testing/UserLogicTest.cs
@@ -7,6 +7,7 @@ using Repository;
 using BusinessLogic;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Testing
 {
@@ -246,7 +247,6 @@ namespace Testing
                 context.SaveChanges();
             }
 
-            Repository.Models.User dbUser;
             using(var context1 = new Cinephiliacs_UserContext(options))
             {
                 context1.Database.EnsureCreated();
@@ -255,7 +255,6 @@ namespace Testing
                 UserLogic test = new UserLogic(userRepo);
                 
                 returnedValue = await Task.Run(() => test.UpdateUser(newUser.Userid, newUser));
-                dbUser = context1.Users.FirstOrDefault(x => x.UserId == oldUser.UserId);
             }
 
             Assert.False(returnedValue);
@@ -308,6 +307,421 @@ namespace Testing
             }
 
             Assert.False(returnedValue);
+        }
+
+        [Fact]
+        public async Task UpdateUser_FalseByEmailExist_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+            Repository.Models.User oldUser1 = new Repository.Models.User();
+            oldUser1.UserId = Guid.NewGuid().ToString();
+            oldUser1.Username = "OldTestingTestington1";
+            oldUser1.FirstName = "OGTest1";
+            oldUser1.LastName = "OriginalTest1";
+            oldUser1.Permissions = 1;
+            oldUser1.Email = "oldtest1@gmail.com";
+            GlobalModels.User newUser = new GlobalModels.User();
+            newUser.Firstname = "Test";
+            newUser.Lastname = "Testing";
+            newUser.Email = oldUser1.Email;
+            newUser.Permissions = 1;
+            newUser.Username = "TestingTestington";
+            newUser.Dateofbirth = "1990-05-20";
+
+            bool returnedValue;
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.Add<Repository.Models.User>(oldUser1);
+                context.SaveChanges();
+            }
+
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                returnedValue = await Task.Run(() => test.UpdateUser(newUser.Userid, newUser));
+            }
+
+            Assert.False(returnedValue);
+        }
+
+        [Fact]
+        public async Task GetUser_True_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.SaveChanges();
+            }
+
+            GlobalModels.User returnUser;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                returnUser = await Task.Run(() => test.GetUser("oldtest@gmail.com"));
+            }
+
+            Assert.Equal(oldUser.UserId, returnUser.Userid);
+        }
+
+        [Fact]
+        public async Task GetUser_UserNull_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.SaveChanges();
+            }
+
+            GlobalModels.User returnUser;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                returnUser = await Task.Run(() => test.GetUser("failtest@gmail.com"));
+            }
+
+            Assert.Null(returnUser);
+        }
+
+        [Fact]
+        public async Task GetUsers_True_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+            Repository.Models.User oldUser1 = new Repository.Models.User();
+            oldUser1.UserId = Guid.NewGuid().ToString();
+            oldUser1.Username = "OldTestingTestington1";
+            oldUser1.FirstName = "OGTest1";
+            oldUser1.LastName = "OriginalTest1";
+            oldUser1.Permissions = 1;
+            oldUser1.Email = "oldtest1@gmail.com";
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.Add<Repository.Models.User>(oldUser1);
+                context.SaveChanges();
+            }
+
+            List<GlobalModels.User> returnUser;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                returnUser = await Task.Run(() => test.GetUsers());
+            }
+
+            Assert.Equal(2, returnUser.Count);
+        }
+
+        [Fact]
+        public async Task DeleteUsers_True_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+            Repository.Models.User oldUser1 = new Repository.Models.User();
+            oldUser1.UserId = Guid.NewGuid().ToString();
+            oldUser1.Username = "OldTestingTestington1";
+            oldUser1.FirstName = "OGTest1";
+            oldUser1.LastName = "OriginalTest1";
+            oldUser1.Permissions = 1;
+            oldUser1.Email = "oldtest1@gmail.com";
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.Add<Repository.Models.User>(oldUser1);
+                context.SaveChanges();
+            }
+
+            bool successDelete;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                successDelete = await Task.Run(() => test.DeleteUser(oldUser.UserId));
+            }
+
+            Assert.True(successDelete);
+        }
+
+        [Fact]
+        public async Task DeleteUsers_False_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+            Repository.Models.User oldUser1 = new Repository.Models.User();
+            oldUser1.UserId = Guid.NewGuid().ToString();
+            oldUser1.Username = "OldTestingTestington1";
+            oldUser1.FirstName = "OGTest1";
+            oldUser1.LastName = "OriginalTest1";
+            oldUser1.Permissions = 1;
+            oldUser1.Email = "oldtest1@gmail.com";
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.Add<Repository.Models.User>(oldUser1);
+                context.SaveChanges();
+            }
+
+            bool successDelete;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                successDelete = await Task.Run(() => test.DeleteUser(Guid.NewGuid().ToString()));
+            }
+
+            Assert.False(successDelete);
+        }
+
+        [Fact]
+        public async Task UpdatePermissions_True_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.SaveChanges();
+            }
+
+            bool successUpdate;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                successUpdate = await Task.Run(() => test.UpdatePermissions(oldUser.UserId, 3));
+            }
+
+            Assert.True(successUpdate);
+        }
+
+        [Fact]
+        public async Task UpdatePermissions_FalseByNullUser_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.SaveChanges();
+            }
+
+            bool successUpdate;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                successUpdate = await Task.Run(() => test.UpdatePermissions(Guid.NewGuid().ToString(), 3));
+            }
+
+            Assert.False(successUpdate);
+        }
+
+        [Fact]
+        public async Task UpdatePermissions_FalseByInvalidPermissionLevelNegavtive_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.SaveChanges();
+            }
+
+            bool successUpdate;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                successUpdate = await Task.Run(() => test.UpdatePermissions(oldUser.UserId, -1));
+            }
+
+            Assert.False(successUpdate);
+        }
+
+        [Fact]
+        public async Task GetUserAge_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+            oldUser.DateOfBirth = DateTime.Parse("1994-05-20");
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.SaveChanges();
+            }
+
+            double? age;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                age = await Task.Run(() => test.GetUserAge(oldUser.UserId));
+            }
+
+            Assert.Equal(26, age);
+        }
+
+        [Fact]
+        public async Task GetUserAge_Null_Test()
+        {
+            Repository.Models.User oldUser = new Repository.Models.User();
+            oldUser.UserId = Guid.NewGuid().ToString();
+            oldUser.Username = "OldTestingTestington";
+            oldUser.FirstName = "OGTest";
+            oldUser.LastName = "OriginalTest";
+            oldUser.Permissions = 1;
+            oldUser.Email = "oldtest@gmail.com";
+            oldUser.DateOfBirth = DateTime.Parse("1994-05-20");
+
+            using(var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add<Repository.Models.User>(oldUser);
+                context.SaveChanges();
+            }
+
+            double? age;
+            using(var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+                
+                RepoLogic userRepo = new RepoLogic(context1);
+                UserLogic test = new UserLogic(userRepo);
+                
+                age = await Task.Run(() => test.GetUserAge(Guid.NewGuid().ToString()));
+            }
+
+            Assert.Null(age);
         }
     }
 }
