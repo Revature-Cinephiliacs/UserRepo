aa9c24f6c8d4dd298cc054cb02c10d81e5dab515
Noureldin Kamel <noureldinashraf6@gmail.com>
2021-05-03 05:20:29 +0300

    updates response values

--- UserApi/Controllers/UserController.cs
+++ UserApi/Controllers/UserController.cs
@@ -70,7 +70,7 @@ namespace CineAPI.Controllers
                     });
                     errorstring += "}";
                 });
-                return this.Accepted(new { error = errorstring });
+                return this.NotFound(new { response = errorstring });
             }
 
             if (await _userLogic.CreateUser(user))
@@ -79,7 +79,7 @@ namespace CineAPI.Controllers
             }
             else
             {
-                return StatusCode(200);
+                return this.Conflict(new { response = "user already created" });
             }
         }
 
